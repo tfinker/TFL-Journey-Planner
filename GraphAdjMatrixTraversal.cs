ae89b49f1c72f724693140b18f3ba5e2a297e0ff
Finlay Kerr <kerrfj@gmail.com>
2022-04-07 13:30:47 +0100

    Path finder done, not sure if very good!

---
+++ GraphAdjMatrixTraversal.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Tutorial_9
+{
+    class GraphAdjMatrixTraversal : GraphAdjMatrix
+    {
+      private bool[] visitedVertex;
+      private List<int> orderTraversed;
+
+      public GraphAdjMatrixTraversal(String graphName, int numberOfVertices)
+               : base(graphName, numberOfVertices)
+        {
+          visitedVertex = new bool[numberOfVertices];
+          orderTraversed = new List<int>();
+        }
+
+        public GraphAdjMatrixTraversal(String graphName, int numberOfVertices, int[,] Edges)
+               : base(graphName, numberOfVertices, Edges)
+        {
+          visitedVertex = new bool[numberOfVertices];
+          orderTraversed = new List<int>();
+
+        }
+
+        public void findDepthPath(int startVertex, int endVertex) {
+          
+          var path = DepthFirstTraversal(startVertex);
+          var specificPath = path.getSpecificPath(startVertex, endVertex);
+          if (specificPath != null) {
+            //specificPath.ForEach(x => Console.Write("( {0},{1} ), ", x.Item1, x.Item2));
+            specificPath.ForEach(x => Console.Write("{0}, ", x));
+            Console.WriteLine();
+
+          }
+        }
+
+        public void findBreadthPath(int startVertex, int endVertex) {
+          
+          var path = BreadthFirstTraversal(startVertex);
+          var specificPath = path.getSpecificPath(startVertex, endVertex);
+          if (specificPath != null) {
+            //specificPath.ForEach(x => Console.Write("( {0},{1} ), ", x.Item1, x.Item2));
+            specificPath.ForEach(x => Console.Write("{0}, ", x));
+            Console.WriteLine();
+
+          }
+        }
+
+        public Path DepthFirstTraversal(int start) {
+
+          Path path = new Path();
+          for(int i = 0; i < numberOfVertices(); i++){
+            visitedVertex[i] = false;
+          }
+
+          //for(int j=0;j<numberOfVertices();j++){
+            //if (!visitedVertex[j]) {
+              DepthFirstSearch(start, path);
+          //  }
+         // }
+          //Console.Write(foreach x in orderTraversed);
+          //orderTraversed.ForEach(x => Console.Write("{0}, ", x));
+          return path;
+        }
+
+        public void DepthFirstSearch(int vertex, Path path){
+          visitedVertex[vertex] = true;
+          orderTraversed.Add(vertex);
+
+          for(int i=0;i<numberOfVertices();i++) {
+            if ((AM[vertex, i] == 1) && (!visitedVertex[i])) {
+              //Console.WriteLine("vertex: {0} i: {1} AM: {2}", vertex, i, AM[vertex, i]);
+              path.Add(vertex, i);
+              DepthFirstSearch(i, path);
+            }
+          }
+        }
+
+        public Path BreadthFirstTraversal(int start) {
+          for(int i = 0; i < numberOfVertices(); i++){
+            visitedVertex[i] = false;
+          }
+          Path path = new Path();
+          List<int> bfsQueue = new List<int> {start};
+
+          while(bfsQueue.Count>0) {
+            visitedVertex[bfsQueue[0]] = true;
+            BreadthFirstSearch(bfsQueue[0], bfsQueue, path);
+          }
+         // orderTraversed.ForEach(x => Console.Write("{0}, ", x));
+          return path;
+        }
+
+        public void BreadthFirstSearch(int vertex, List<int> queue, Path path){
+          for(int i =0; i<numberOfVertices(); i++){
+            if ((AM[vertex,i] == 1) && (!visitedVertex[i])){
+              queue.Add(i);
+              visitedVertex[i] = true;
+              path.Add(vertex, i);
+              //Console.WriteLine("vertex: {0}, i: {1}", vertex, i);
+            }
+          }
+          queue.RemoveAt(0);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Tutorial_9
+{
+    class TestingGraphs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine("//              Testing Graphs                 //") ;
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine();
+
+            const String name             = "Week 9 Graph";
+            const int    numberOfVertices = 8 ;
+
+            int[,] Edges = new int[,] { { 0, 1 }, { 0, 2 }, { 0, 3 },
+                                        { 1, 0 }, { 1, 2 }, { 1, 3 },{ 1, 5}, 
+                                        { 2, 1 }, { 2, 5 }, { 2, 6 }, 
+                                        { 3, 0 }, { 3, 1 }, { 3, 7 },  
+                                        { 4, 2 }, 
+                                        //{ 5, 6 }, { 5, 7 },
+                                        { 6, 2, }, { 6, 4}
+                                      } ;
+
+
+            Console.WriteLine("/////////////////////////////////////////////////////");
+            Console.WriteLine("// Create Undirected Graph using Adjacency Matrix  //");
+            Console.WriteLine("/////////////////////////////////////////////////////");
+            Console.WriteLine();
+
+            // TO DO:  
+            // Create the graph 
+
+            // var graph = new GraphAdjMatrix(name, numberOfVertices, Edges);
+            // graph.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine("//    Test Undirected Graph methods            //");
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine();
+
+            // TO DO:  
+            // Test its methods 
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("//////////////////////////////////////////////////");
+            Console.WriteLine("// Create Undirected Graph using Adjacency List //");
+            Console.WriteLine("//////////////////////////////////////////////////");
+            Console.WriteLine();
+
+            // TO DO:  
+            // Create the graph 
+
+            Console.WriteLine();
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine("//    Test Graph methods                       //");
+            Console.WriteLine("/////////////////////////////////////////////////");
+            Console.WriteLine();
+
+            // TO DO:  
+            // Test its methods          
+
+
+            var graph = new GraphAdjMatrixTraversal(name, numberOfVertices, Edges);
+            //graph.DepthFirstTraversal(6);
+
+            graph.findDepthPath(7,0);
+            graph.findBreadthPath(7,0);
+        }
+    }
+}
